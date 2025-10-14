d876535a1cfafe81493dfae91fcb752551e0b0b3
IEVN1001-22001567 <81333@alumnos.utleon.edu.mx>
2025-10-13 20:42:53 -0600

    Rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DistanciaComponent } from './formularios/distancia/distancia.component';
+import { MultiplicacionComponent } from './formularios/multiplicacion/multiplicacion.component';
+import { AporBComponent } from './formularios/apor-b/apor-b.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DistanciaComponent, MultiplicacionComponent, AporBComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/formularios/distancia/distancia.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { OperacionesDistancia } from './distanciaoperaciones';
+
+@Component({
+  selector: 'app-distancia',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './distancia.component.html',
+  styleUrl: './distancia.component.css'
+})
+export class DistanciaComponent {
+  formulario!:FormGroup;
+  objDist=new OperacionesDistancia();
+  resultado!:number;
+
+   constructor() {}
+
+  ngOnInit(): void {
+    this.formulario = new FormGroup({
+      x1: new FormControl(''),
+      y1: new FormControl(''),
+      x2: new FormControl(''),
+      y2: new FormControl('')
+    });
+  }
+   
+  Distancia(): void {
+    this.objDist.x1= this.formulario.value.x1;
+    this.objDist.x2= this.formulario.value.x2;
+    this.objDist.y1= this.formulario.value.y1;
+    this.objDist.y2= this.formulario.value.y2;
+
+  this.objDist.calcularDistancia();
+  this.resultado= this.objDist.resultado;
+      
+  }
+}
